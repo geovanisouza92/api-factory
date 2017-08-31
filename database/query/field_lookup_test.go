cc141a1c6489d036def4ce4768ee3259a92a7429
Geovani de Souza <geovani@serrabits.com.br>
2017-08-31 08:44:37 -0300

    Remove goconvey

--- database/query/field_lookup_test.go
+++ database/query/field_lookup_test.go
@@ -3,49 +3,57 @@ package query
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/smartystreets/assertions"
 )
 
-func TestFieldLookup(t *testing.T) {
-	Convey("Field lookups translate as expected", t, func() {
-		Convey("example 1", func() {
-			expected := FieldLookup{
-				op:      Exact,
-				field:   "id",
-				lookups: []string{},
-			}
-			actual := NewFieldLookup("id")
-			So(actual, ShouldResemble, expected)
-		})
+func TestFieldLookup1(t *testing.T) {
+	expected := FieldLookup{
+		op:      Exact,
+		field:   "id",
+		lookups: []string{},
+	}
+	actual := NewFieldLookup("id")
 
-		Convey("example 2", func() {
-			expected := FieldLookup{
-				op:      Lt,
-				field:   "id",
-				lookups: []string{},
-			}
-			actual := NewFieldLookup("id__lt")
-			So(actual, ShouldResemble, expected)
-		})
+	if msg := assertions.ShouldResemble(actual, expected); msg != "" {
+		t.Error(msg)
+	}
+}
 
-		Convey("example 3", func() {
-			expected := FieldLookup{
-				op:      StartsWith,
-				field:   "name",
-				lookups: []string{"author"},
-			}
-			actual := NewFieldLookup("author__name__startswith")
-			So(actual, ShouldResemble, expected)
-		})
+func TestFieldLookup2(t *testing.T) {
+	expected := FieldLookup{
+		op:      Lt,
+		field:   "id",
+		lookups: []string{},
+	}
+	actual := NewFieldLookup("id__lt")
 
-		Convey("example 4", func() {
-			expected := FieldLookup{
-				op:      Exact,
-				field:   "name",
-				lookups: []string{"blog", "author"},
-			}
-			actual := NewFieldLookup("blog__author__name")
-			So(actual, ShouldResemble, expected)
-		})
-	})
+	if msg := assertions.ShouldResemble(actual, expected); msg != "" {
+		t.Error(msg)
+	}
+}
+
+func TestFieldLookup3(t *testing.T) {
+	expected := FieldLookup{
+		op:      StartsWith,
+		field:   "name",
+		lookups: []string{"author"},
+	}
+	actual := NewFieldLookup("author__name__startswith")
+
+	if msg := assertions.ShouldResemble(actual, expected); msg != "" {
+		t.Error(msg)
+	}
+}
+
+func TestFieldLookup4(t *testing.T) {
+	expected := FieldLookup{
+		op:      Exact,
+		field:   "name",
+		lookups: []string{"blog", "author"},
+	}
+	actual := NewFieldLookup("blog__author__name")
+
+	if msg := assertions.ShouldResemble(actual, expected); msg != "" {
+		t.Error(msg)
+	}
 }
--- database/query/query_test.go
+++ database/query/query_test.go
@@ -2,12 +2,8 @@ package query
 
 import (
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestQuery(t *testing.T) {
-	Convey("Query", t, func() {
-		// TODO:
-	})
+	// TODO:
 }
