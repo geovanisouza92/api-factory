8e0140e9331e8d22d70f1e152b7f995230d62ffa
Geovani de Souza <geovanisouza92@gmail.com>
2017-09-05 22:22:38 -0300

    Change a little the nodejs plugin

--- builtins/bins/api-factory-scripting-nodejs/index.js
+++ builtins/bins/api-factory-scripting-nodejs/index.js
@@ -2,19 +2,35 @@
 
 // const vm2 = require("vm2");
 const grpc = require("grpc");
-const messages = require("./proto/scripting_pb");
-const services = require("./proto/scripting_grpc_pb");
-
-const run = () => {};
+const messages = {
+  health: require("./proto/health_pb"),
+  scripting: require("./proto/scripting_pb")
+};
+const services = {
+  health: require("./proto/health_grpc_pb"),
+  scripting: require("./proto/scripting_grpc_pb")
+};
 
 const getServer = bindAddress => {
   const server = new grpc.Server();
-  server.addService(services.ScriptingProviderService, {
-    run
+  server.addService(services.health.HealthService, {
+    check(call, callback) {
+      const reply = new messages.health.HealthCheckResponse();
+      reply.setStatus(
+        messages.health.HealthCheckResponse.ServingStatus.SERVING
+      );
+      callback(null, reply);
+    }
+  });
+  server.addService(services.scripting.ScriptingProviderService, {
+    run(call, callback) {}
   });
   server.bind(bindAddress, grpc.ServerCredentials.createInsecure());
   return server;
 };
 
-const server = plugin.getServer("localhost:50051");
+const NETWORK_ADDR = process.env.NETWORK_ADDR || "127.0.0.1:50051";
+
+const server = getServer(NETWORK_ADDR);
 server.start();
+console.log(`1|${process.env.APP_PROTOCOL_VERSION}|tcp|${NETWORK_ADDR}|grpc`);
